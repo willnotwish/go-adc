9eb74546b5ed2885cc82f740247d99eac2284239
Nick Adams <nick@nickadams.co.uk>
2019-04-12 13:59:30 +0100

    Added pusher

--- serial/src/main.js
+++ serial/src/main.js
@@ -1,6 +1,16 @@
-const OBDReader = require('serial-obd');
+const OBDReader = require('serial-obd')
+const Pusher = require('pusher')
 
 const log = (msg, ...params) => console.log("main. " + msg, ...params)
+
+const pusher = new Pusher({
+  appId:    process.env.PUSHER_APP_ID,
+  key:      process.env.PUSHER_APP_KEY,
+  secret:   process.env.PUSHER_APP_SECRET,
+  cluster:  process.env.PUSHER_APP_CLUSTER,
+  encryted: true
+})
+log("Pusher client created")
 
 const options = {}
 options.baudRate = 38400
@@ -9,10 +19,10 @@ log("About to create OBDReader with: ", process.env.DEVICE)
 const serialOBDReader = new OBDReader(process.env.DEVICE, options)
 
 let dataReceivedMarker = {}
-
 serialOBDReader.on('dataReceived', data => {
   console.log(data)
   dataReceivedMarker = data
+  pusher.trigger('obd', 'data-received', data)
 })
 
 serialOBDReader.on('connected', function(data) {
