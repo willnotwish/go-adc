96d699e8141579f7c236a80632faddb7f63ea396
Nick Adams <nick@nickadams.co.uk>
2019-04-12 12:19:02 +0100

    First attempt at OBD

--- serial/src/main.js
+++ serial/src/main.js
@@ -1,16 +1,67 @@
-const SerialPort = require('serialport')
+const OBDReader = require('serial-obd');
 
-const sp = new SerialPort(process.env.DEVICE)
+const log = (msg, ...params) => console.log("main. " + msg, ...params)
 
-sp.on( 'error', (err) => {
-  console.log("Error: ", err)
+const options = {}
+options.baudRate = 38400
+
+log("About to create OBDReader with: ", process.env.DEVICE)
+const serialOBDReader = new OBDReader(process.env.DEVICE, options)
+
+let dataReceivedMarker = {}
+
+serialOBDReader.on('dataReceived', data => {
+  console.log(data)
+  dataReceivedMarker = data
 })
 
-sp.on( 'open', console.log )
+serialOBDReader.on('connected', function(data) {
+  log("Connected. Adding pollers...")
+  this.addPoller("vss")
+  this.addPoller("rpm")
+  this.addPoller("temp")
+  this.addPoller("load_pct")
+  this.addPoller("map")
+  this.addPoller("frp")
 
-sp.write("AAABBBCCC\n")
-sp.write("from Nick\n")
-
-sp.on( 'data', data => {
-  console.log("Data received: ", data)
+  this.startPolling(2000)
 })
+
+log("About to connect to vehicle")
+serialOBDReader.connect()
+
+// const SerialPort = require('serialport')
+
+// const log = (msg, ...params) => {
+//   console.log("main. " + msg, params)
+// }
+
+// log("About to create serial port from device ", process.env.DEVICE)
+// const sp = new SerialPort(process.env.DEVICE)
+
+// sp.on( 'error', (err) => {
+//   log("Error: ", err)
+// })
+
+// sp.on( 'open', () => log("Serial port opened OK") )
+
+// const msg = "AAABBBCCCDDDEEEFFF\n"
+
+// sp.write(msg)
+// log("Sent: ", msg)
+
+// sp.write(msg)
+// log("Sent: ", msg)
+
+// sp.write(msg)
+// log("Sent: ", msg)
+
+// sp.write(msg)
+// log("Sent: ", msg)
+
+// sp.write(msg)
+// log("Sent: ", msg)
+
+// sp.on( 'data', data => {
+//   log("Data received: ", data)
+// })
